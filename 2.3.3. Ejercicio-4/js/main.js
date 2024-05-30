7bee0bcb38b1162b277a6aef20d39c42382d9c7c
Ale <alexlab84@hotmail.com>
2024-05-30 18:00:00 +0200

    I add exercise 4 conditionals completed

--- 2.3.3. Ejercicio-4/js/main.js
+++ 2.3.3. Ejercicio-4/js/main.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const age = document.querySelector('.dog_age');
+  
 const btn = document.querySelector('.btn');
 const result = document.querySelector('.result');
 
 btn.addEventListener('click', (ev) => {
     ev.preventDefault();
     
-    if (age.value === '1') {
+    const ageInt = parseInt(age.value);
+    
+    if (ageInt === 1) {
         result.innerHTML = 'Tu perro tiene 15 años humanos';
-    } else if (age.value === '2') {
+    } else if (ageInt === 2) {
         result.innerHTML = 'Tu perro tiene 24 años humanos';
-    } else if (age.value >= '3') { 
-        const mayorDeTres = 24 + (age.value -2) *5;
+    } else if (ageInt >= 3) { 
+        const mayorDeTres = 24 + (ageInt -2) *5;
         result.innerHTML = 'Tu perro tiene ' + mayorDeTres + ' años humanos';
 
     } else {
