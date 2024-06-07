f8fa379872f650a45d52e1c1fdad244ff76d8a3d
Ale <alexlab84@hotmail.com>
2024-06-07 20:14:27 +0200

    add pear basket exercise done with the teacher

--- 2.7.2 Ejercicio 2/js/main.js
+++ 2.7.2 Ejercicio 2/js/main.js
@@ -6,22 +6,62 @@ const cestaPeras = {
     actual: 0,
     inicial: 0,
     add: function (num) {
-        const add = this.inicial + num;
-        this.actual = this.inicial + num; 
-        return add;
+        //const add = this.inicial + num;
+        
+        if (this.actual + num > this.max) {
+            console.log('Te has pasao');
+            return;
+        } else {
+            this.actual = this.actual + num;
+        }
+
+         
+        //return add;
     },
     remove: function (num) {
-        return this.add(this.actual) - num;
+
+        
+        if (this.actual - num < this.min) {
+            console.log ('No llegas');
+        } else {
+            this.actual = this.actual - num;
+            console.log ('Sigue su curso peril')
+        }
+
+        //this.actual -= num;
+        //const actulizar = this.actual - num;
+
+        //return this.add(this.actual) - num;
     },
     reset: function () {
-        return this.inicial;
+        this.actual = this.inicial; 
     }
 };
 
+console.log(cestaPeras);
+debugger;
+console.log('cestaPeras.add(80);')
+cestaPeras.add(80);
 
-console.log(cestaPeras.add(80));
-console.log(cestaPeras.remove(10));
-console.log(cestaPeras.reset());
+console.log(cestaPeras);
+
+console.log('cestaPeras.add(50);')
+cestaPeras.add(5000);
+
+console.log(cestaPeras);
+
+console.log('cestaPeras.remove(5000)');
+cestaPeras.remove(5000);
+
+console.log(cestaPeras);
+
+/* console.log(cestaPeras.add(80)); */
+
+
+console.log('cestaPeras.reset()');
+cestaPeras.reset();
+
+console.log(cestaPeras);
 
 
 
